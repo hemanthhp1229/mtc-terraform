ec690660a8928bbba765678a6bc6b9b61ef09f17
EC2 Default User <ec2-user@ip-172-31-28-15.ec2.internal>
2023-06-17 04:30:25 +0000

    created docker container and docker image. and deployed it. Also it was available on the web

--- 02-terraform-basics-docker/02-Terraform-Init-Deeper-Dive/main.tf
+++ 02-terraform-basics-docker/02-Terraform-Init-Deeper-Dive/main.tf
@@ -13,3 +13,13 @@ provider "docker" {}
 resource "docker_image" "mynodered_image" {
   name = "nodered/node-red"
 }
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.mynodered_image.image_id
+  ports {
+    internal = 1880
+    external = 1880
+    protocol = "tcp"
+  }
+}
